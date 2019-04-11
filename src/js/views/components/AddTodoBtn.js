def87f965ffcf07a5ba4c44b27b1f93a9f6be671
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-11 18:07:23 +0600

    Add todo btn display block

--- src/js/views/components/AddTodoBtn.js
+++ src/js/views/components/AddTodoBtn.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props) => {
   const { handleTodoBtn, addTodoMode } = props;
-  const displayClass = !addTodoMode ? 'btn btn-primary' : 'd-none';
+  const displayClass = !addTodoMode ? 'btn btn-primary btn-block' : 'd-none';
   return (
     <button type="button" className={displayClass} onClick={handleTodoBtn}>
       Add Todo
